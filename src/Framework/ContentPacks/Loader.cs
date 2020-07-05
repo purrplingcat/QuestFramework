6f43785d15c28e428d8ff5261a84fcbf56fc1839
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2020-07-05 18:46:25 +0200

    Initial commit

---
+++ src/Framework/ContentPacks/Loader.cs
@@ -0,0 +1,145 @@
+﻿using QuestFramework.Framework.ContentPacks.Model;
+using QuestFramework.Quests;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestFramework.Framework.ContentPacks
+{
+    class Loader
+    {
+        public Loader(IMonitor monitor, QuestManager manager, QuestOfferManager scheduleManager)
+        {
+            this.Contents = new List<Content>();
+            this.ValidContents = new List<Content>();
+            this.Monitor = monitor;
+            this.Manager = manager;
+            this.ScheduleManager = scheduleManager;
+        }
+
+        public List<Content> Contents { get; }
+        public List<Content> ValidContents { get; }
+        public IMonitor Monitor { get; }
+        public QuestManager Manager { get; }
+        public QuestOfferManager ScheduleManager { get; }
+
+        public void LoadPacks(IEnumerable<IContentPack> contentPacks)
+        {
+            int count = 0;
+            foreach (var contentPack in contentPacks)
+            {
+                var content = this.LoadContentPack(contentPack);
+
+                if (content != null && this.Validate(content))
+                {
+                    this.ValidContents.Add(content);
+                    ++count;
+                }
+            }
+
+            this.Monitor.Log($"Loaded {count} content packs.", LogLevel.Info); 
+        }
+
+        private void Prepare(Content content)
+        {
+            foreach (var schedule in content.Offers)
+            {
+                if (!schedule.QuestName.Contains('@'))
+                {
+                    schedule.QuestName = $"{schedule.QuestName}@{content.owner.Manifest.UniqueID}";
+                }
+            }
+        }
+
+        private bool Validate(Content content)
+        {
+            bool isValid = true;
+
+            if (content.Format == null || content.Format.IsOlderThan("1.0"))
+            {
+                this.Monitor.Log($"Content pack `{content.owner.Manifest.Name}` has unsupported format version {content.Format}.", LogLevel.Error);
+                isValid = false;
+            }
+
+            if (content.Quests == null || !content.Quests.Any())
+            {
+                this.Monitor.Log($"Content pack `{content.owner.Manifest.Name}` contains no quests.", LogLevel.Error);
+                isValid = false;
+            }
+
+            return isValid;
+        }
+
+        private void Apply(Content content)
+        {
+            // Register quests
+            foreach (var quest in content.Quests)
+            {
+                CustomQuest managedQuest = MapQuest(content, quest);
+
+                if (quest.Hooks != null)
+                {
+                    managedQuest.Hooks.AddRange(quest.Hooks);
+                }
+
+                this.Manager.RegisterQuest(managedQuest);
+            }
+
+            // Add quest schedules
+            foreach (var offer in content.Offers)
+            {
+                this.ScheduleManager.AddOffer(offer);
+            }
+        }
+
+        public void RegisterQuestsFromPacks()
+        {
+            this.ValidContents.ForEach(content => this.Apply(content));
+
+        }
+
+        private static CustomQuest MapQuest(Content content, Quest quest)
+        {
+            var managedQuest = new CustomQuest(quest.Name)
+            {
+                Title = quest.Title,
+                Description = quest.Description,
+                BaseType = quest.Type,
+                Objective = quest.Objective,
+                Reward = quest.Reward,
+                RewardDescription = quest.RewardDescription,
+                ReactionText = quest.ReactionText,
+                Cancelable = quest.Cancelable,
+                Trigger = quest.Trigger,
+                NextQuests = quest.NextQuests,
+                OwnedByModUid = content.owner.Manifest.UniqueID,
+            };
+
+            if (quest.CustomTypeId != -1)
+            {
+                managedQuest.CustomTypeId = quest.CustomTypeId;
+            }
+
+            return managedQuest;
+        }
+
+        private Content LoadContentPack(IContentPack contentPack)
+        {
+            if (!contentPack.HasFile("quests.json"))
+            {
+                this.Monitor.Log($"Content pack `{contentPack.Manifest.Name}` has no entry file `quests.json`", LogLevel.Error);
+                return null;
+            }
+
+            var content = contentPack.ReadJsonFile<Content>("quests.json");
+
+            content.owner = contentPack;
+            this.Prepare(content);
+
+            return content;
+        }
+    }
+}
---
+++ src/Framework/ContentPacks/Model/Quest.cs
@@ -0,0 +1,26 @@
+﻿using QuestFramework.Hooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestFramework.Framework.ContentPacks.Model
+{
+    class Quest
+    {
+        public string Name { get; set; }
+        public QuestType Type { get; set; } = QuestType.Basic;
+        public int CustomTypeId { get; set; } = -1;
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Objective { get; set; }
+        public List<string> NextQuests { get; set; }
+        public int Reward { get; set; }
+        public string RewardDescription { get; set; }
+        public bool Cancelable { get; set; }
+        public string ReactionText { get; set; }
+        public object Trigger { get; set; }
+        public List<Hook> Hooks { get; set; }
+    }
+}
