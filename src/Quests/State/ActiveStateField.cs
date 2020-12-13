ccf867bdf65ff399b18d18fbd7adda95a3039495
PurrplingCat <git@purrplingcat.com>
2020-12-13 19:03:00 +0100

    minor edits

--- src/Quests/State/ActiveStateField.cs
+++ src/Quests/State/ActiveStateField.cs
@@ -1,9 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuestFramework.Quests.State
 {
@@ -22,15 +18,13 @@ namespace QuestFramework.Quests.State
             }
         }
 
-        protected abstract void Reset(bool silent);
         public Action<ActiveStateField> OnChange { get; internal set; }
         public abstract JToken ToJToken();
         public abstract void FromJToken(JToken token);
-        public void Reset() => this.Reset(silent: false);
-        internal void SilentReset() => this.Reset(silent: true);
+        public abstract void Reset();
     }
 
-    public class ActiveStateField<T> : ActiveStateField
+    public sealed class ActiveStateField<T> : ActiveStateField
     {
         private readonly T _defaultValue;
         private T _value;
@@ -67,14 +61,10 @@ namespace QuestFramework.Quests.State
             return JToken.FromObject(this._value);
         }
 
-        protected override void Reset(bool silent)
+        public override void Reset()
         {
             this._value = this._defaultValue;
-
-            if (!silent)
-            {
-                this.OnChange?.Invoke(this);
-            }
+            this.OnChange?.Invoke(this);
         }
 
         public override void FromJToken(JToken token)
