af7f527a6a8358ec6914c42f3a97acaf809facf2
PurrplingCat <ellenfawkes@gmail.com>
2021-05-30 10:31:45 +0200

    Add casting extensions for ICompletionMessage

---
+++ src/Extensions/CompletionMessageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using QuestFramework.Quests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestFramework.Extensions
+{
+    public static class CompletionMessageExtensions
+    {
+        public static T Cast<T>(this ICompletionMessage completionMessage)
+        {
+            if (completionMessage is T _completionMessage)
+            {
+                return _completionMessage;
+            }
+
+            return JObject.FromObject(completionMessage).ToObject<T>();
+        }
+
+        public static object[] ToArray(this ICompletionMessage completionMessgage)
+        {
+            return JObject.FromObject(completionMessgage).AsJEnumerable().ToArray();
+        }
+    }
+}
